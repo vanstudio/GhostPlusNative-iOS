4c33c0a27c9c37c94d1382059bbed9eb7829fe92
VANSTUDIO <vanstudio@daum.net>
2018-02-08 18:15:08 +0900

    Release 1.07 - GPNDynamicBannerView itemSpacing bug fixed

--- Frameworks/GhostPlusNative.framework/Versions/A/Headers/GhostPlusNative.h
+++ Frameworks/GhostPlusNative.framework/Versions/A/Headers/GhostPlusNative.h
@@ -23,8 +23,8 @@
 #import <GhostPlusNative/GPNGridView.h>
 
 // Constans
-#define GhostPlusNative_VERSION           @"1.06";
-#define GhostPlusNative_BUILD_VERSION     @"106";
+#define GhostPlusNative_VERSION           @"1.07";
+#define GhostPlusNative_BUILD_VERSION     @"107";
 
 
 /**
--- GhostPlusNative.podspec
+++ GhostPlusNative.podspec
@@ -1,4 +1,4 @@
-@version = "1.06"
+@version = "1.07"
 Pod::Spec.new do |s|
   s.name             = 'GhostPlusNative'
   s.version          = @version
