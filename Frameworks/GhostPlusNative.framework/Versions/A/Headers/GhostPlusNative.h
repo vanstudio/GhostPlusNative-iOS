7b526079b489a195d7b4dfe2d30f1e07e08d8f0b
VANSTUDIO <vanstudio@daum.net>
2020-07-22 20:22:34 +0900

    - GPNDynamicBannerView update

--- Frameworks/GhostPlusNative.framework/Versions/A/Headers/GhostPlusNative.h
+++ Frameworks/GhostPlusNative.framework/Versions/A/Headers/GhostPlusNative.h
@@ -23,8 +23,8 @@
 #import <GhostPlusNative/GPNGridView.h>
 
 // Constans
-#define GhostPlusNative_VERSION           @"1.22";
-#define GhostPlusNative_BUILD_VERSION     @"122";
+#define GhostPlusNative_VERSION           @"1.23";
+#define GhostPlusNative_BUILD_VERSION     @"123";
 
 
 /**
